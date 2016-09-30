55c99f08211b604012f11792067de25ba3544265
kuyur <kuyur@kuyur.info>
2016-09-30 16:17:27 +0900

    change to LGPL v3 license.

--- c4charmapmaker-simp2tra/c4charmapmaker-simp2tra.cpp
+++ c4charmapmaker-simp2tra/c4charmapmaker-simp2tra.cpp
@@ -5,8 +5,8 @@
 /*                                                                      */
 /* Version: 1.0                                                         */
 /* Author:  kuyur (kuyur@kuyur.info)                                    */
-/* Published under Apache License 2.0                                   */
-/* http://www.apache.org/licenses/LICENSE-2.0                           */
+/* Published under LGPL-3.0                                             */
+/* https://www.gnu.org/licenses/lgpl-3.0.en.html                        */
 /*                                                                      */
 /* Copyright 2016 Project c4                                            */
 /* https://github.com/kuyur/c4                                          */
